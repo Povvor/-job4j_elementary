9acbb27cb8077b60ff09f3e8183ebf4e7df6b7f8
Povvor <Povvor@yandex.ru>
2025-03-17 16:02:42 +0300

    Выход за границу массива [#505229]

---
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class SwitchArray {
+
+    public static int[] swapBorder(int[] array) {
+        int temp = array[0];
+        int last = array.length - 1;
+        array[0] = array[last];
+        array[last] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
+        int[] result = swapBorder(numbers);
+        for (int index = 0; index < result.length; index++) {
+            System.out.println(result[index]);
+        }
+    }
+}
