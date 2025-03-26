fc1d906035f7ea109308bf56f477bd7c130a30e4
Povvor <Povvor@yandex.ru>
2025-03-27 01:29:43 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,33 +7,37 @@ class PointTest {
 
     @Test
     void whenPoints00And20Then2() {
-        int x1 = 0, y1 = 0, x2 = 2, y2 = 0;
+        Point pointA = new Point(0, 0);
+        Point pointB = new Point(2, 0);
         double expected = 2.0;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = pointA.distance(pointB);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus1Minus1And1And1Then2Dot83() {
-        int x1 = -1, y1 = -1, x2 = 1, y2 = 1;
+        Point pointA = new Point(-1, -1);
+        Point pointB = new Point(1, 1);
         double expected = 2.83;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = pointA.distance(pointB);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPointsMinus2Minus2And2And2Then5Dot66() {
-        int x1 = -2, y1 = -2, x2 = 2, y2 = 2;
+        Point pointA = new Point(-2, -2);
+        Point pointB = new Point(2, 2);
         double expected = 5.66;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = pointA.distance(pointB);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenPoints11And45Then5() {
-        int x1 = 2, y1 = 2, x2 = 5, y2 = 6;
+        Point pointA = new Point(1, 1);
+        Point pointB = new Point(4, 5);
         double expected = 5;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = pointA.distance(pointB);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
